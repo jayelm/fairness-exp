c67727a5b75ee79a49eb432723c1f9c461a00957
Jesse Mu <6245103+jayelm@users.noreply.github.com>
2020-02-27 18:21:13 -0500

    Update

--- code/main.py
+++ code/main.py
@@ -2,14 +2,15 @@
 Fairness experiments
 """
 
+import os
 
 import numpy as np
+import pandas as pd
 import torch
 from torch import nn, optim
 from torch.utils.data import DataLoader
 
-
-N_HIDDEN = 64
+from tqdm import trange
 
 
 class MLP(nn.Module):
@@ -20,7 +21,7 @@ class MLP(nn.Module):
 
     def forward(self, inp):
         rep = self.l1(inp)
-        out = self.l2(rep)
+        out = self.l2(rep).squeeze(1)
         return rep, out
 
 
@@ -70,14 +71,15 @@ def load(fname, hide_features=("sex", "race")):
         CatFeature("native-country", sorted(types[13])),
     ]
 
-    rows = []
+    xs = []
+    xs_hidden = []
     ys = []
     with open(fname) as reader:
         for line in reader:
             values = line.strip().split(",")
             if len(values) < n_features:
                 continue
-            rows.append(
+            xs_hidden.append(
                 np.concatenate(
                     [
                         feature(val.strip())
@@ -86,40 +88,60 @@ def load(fname, hide_features=("sex", "race")):
                     ]
                 )
             )
+            xs.append(
+                np.concatenate(
+                    [
+                        feature(val.strip())
+                        for feature, val in zip(features, values[:-1])
+                    ]
+                )
+            )
             ys.append(0 if values[-1].strip() == "<=50K" else 1)
-    xs = np.array(rows, dtype=np.float32)
+
+    xs = np.array(xs, dtype=np.float32)
+    xs_hidden = np.array(xs_hidden, dtype=np.float32)
     ys = np.array(ys)
+
     mean = np.mean(xs, axis=0, keepdims=True)
     std = np.std(xs, axis=0, keepdims=True)
     xs = (xs - mean) / std
-    return xs, mean, std, ys, features
+
+    mean_hidden = np.mean(xs_hidden, axis=0, keepdims=True)
+    std_hidden = np.std(xs_hidden, axis=0, keepdims=True)
+    xs_hidden = (xs_hidden - mean_hidden) / std_hidden
+
+    return xs, xs_hidden, ys, features
 
 
 def train(args):
-    xs, mean, std, ys, features = load(args.train_data)
-    loader = DataLoader(list(zip(xs, ys)), batch_size=100, shuffle=True)
+    xs, xs_hidden, ys, features = load(args.train_data)
+    loader = DataLoader(
+        list(zip(xs_hidden, ys)), batch_size=args.batch_size, shuffle=True
+    )
 
-    model = MLP(xs.shape[1], N_HIDDEN, 2)
-    loss_fn = nn.CrossEntropyLoss()
+    model = MLP(xs_hidden.shape[1], args.n_hidden, 1)
+    loss_fn = nn.BCEWithLogitsLoss()
     opt = optim.Adam(model.parameters(), lr=0.0001)
 
-    for i in range(10):
+    ranger = trange(args.epochs, desc=f"epoch 0")
+    for i in ranger:
         epoch_loss = 0
         epoch_acc = 0
         batch_count = 0
         for batch_xs, batch_ys in loader:
             _, pred = model(batch_xs)
-            loss = loss_fn(pred, batch_ys)
+            loss = loss_fn(pred, batch_ys.float())
             opt.zero_grad()
             loss.backward()
             opt.step()
             epoch_loss += loss.item()
-            acc = (pred.argmax(dim=1) == batch_ys).float().mean().item()
+            acc = ((pred > 0) == batch_ys).float().mean().item()
             epoch_acc += acc
             batch_count += 1
-        print(epoch_loss / batch_count)
-        print(epoch_acc / batch_count)
-        print()
+
+        avg_loss = epoch_loss / batch_count
+        avg_acc = epoch_acc / batch_count
+        ranger.set_description(f"epoch {i} loss: {avg_loss:.3f}, acc: {avg_acc:.3f}")
 
     torch.save(model.state_dict(), args.save_model)
 
@@ -137,77 +159,116 @@ def names(features):
     return out
 
 
-def compute_derived_features(names, masks):
-    n_comb = masks.shape[1] * (masks.shape[1] - 1) // 2
-    conjunctions = np.zeros((masks.shape[0], n_comb))
-    disjunctions = np.zeros((masks.shape[0], n_comb))
-    conj_names = []
-    disj_names = []
-    counter = 0
-    for i in range(masks.shape[1]):
-        for j in range(i + 1, masks.shape[1]):
-            conjunctions[:, counter] = np.minimum(masks[:, i], masks[:, j])
-            disjunctions[:, counter] = np.maximum(masks[:, i], masks[:, j])
-            conj_names.append(names[i] + " AND " + names[j])
-            disj_names.append(names[i] + " OR " + names[j])
-            counter += 1
-    return disj_names, disjunctions
+def compute_features(base_names, base_masks, max_length):
+    names = base_names[:]
+    masks = [base_masks[:, i] for i in range(len(names))]
+
+    for i in range(max_length - 1):
+        masks_to_add = []
+        names_to_add = []
+
+        for name, mask in zip(names, masks):
+            for j, name_to_add in enumerate(base_names):
+                if name_to_add == name:
+                    continue
+                mask_to_add = base_masks[:, j]
+                disj_mask = mask | mask_to_add
+                disj_name = f"({name}) OR ({name_to_add})"
+
+                conj_mask = mask & mask_to_add
+                conj_name = f"({name}) AND ({name_to_add})"
+
+                masks_to_add.append(disj_mask)
+                names_to_add.append(disj_name)
+
+        names.extend(names_to_add)
+        masks.extend(masks_to_add)
+
+    masks = np.stack(masks, axis=1)
+    return names, masks
+
+
+def mask_threshold(feats, threshold):
+    thresholds = np.quantile(feats, 1 - threshold, axis=0, keepdims=True)
+    return feats > thresholds
 
 
 def analyze(args):
-    xs, _, _, ys, features = load(args.train_data)
-    n_ref = 2000
-    ref_xs = xs[:n_ref, :]
-    ref_ys = ys[:n_ref]
+    xs, xs_hidden, ys, features = load(args.train_data)
+
+    ref_xs = xs[: args.n_explain, :]
+    ref_xs_hidden = xs_hidden[: args.n_explain, :]
+    ref_ys = ys[: args.n_explain]
 
     feature_names = names(features)
+    #  feature_masks = mask_threshold(ref_xs, args.feature_threshold)
     feature_masks = ref_xs > 0
-    derived_feature_names, derived_feature_masks = compute_derived_features(
-        feature_names, feature_masks,
+    feature_names, feature_masks = compute_features(
+        feature_names, feature_masks, args.max_formula_length
     )
 
-    all_feature_names = feature_names + derived_feature_names
-    all_feature_masks = np.concatenate((feature_masks, derived_feature_masks), axis=1)
-    # all_feature_names = feature_names
-    # all_feature_masks = feature_masks
+    loader = DataLoader(list(zip(ref_xs_hidden, ref_ys)), batch_size=args.batch_size)
 
-    loader = DataLoader(list(zip(ref_xs, ref_ys)), batch_size=100)
-
-    model = MLP(xs.shape[1], N_HIDDEN, 2)
+    model = MLP(xs_hidden.shape[1], args.n_hidden, 1)
     model.load_state_dict(torch.load(args.save_model))
-    neuron_masks = np.zeros((n_ref, N_HIDDEN), dtype=np.byte)
+
+    accs = []
+
+    # Get neuron activations
+    neuron_acts = np.zeros((args.n_explain, args.n_hidden), dtype=np.float32)
     for i_batch, (batch_xs, batch_ys) in enumerate(loader):
         with torch.no_grad():
             reps, preds = model(batch_xs)
         reps = reps.cpu().numpy()
-        pred_labels = preds.argmax(axis=1)
+        pred_labels = preds > 0
         acc = (pred_labels == batch_ys).float().mean().item()
-        active = reps > 0
-        neuron_masks[i_batch * 100 : (i_batch + 1) * 100, :] = active
+        accs.append(acc)
 
-    out_weights = list(model.parameters())[-2].detach().numpy()
-    assert out_weights.shape == (2, N_HIDDEN)
-    explain(list(range(N_HIDDEN)), neuron_masks, all_feature_masks, all_feature_names)
+        start = i_batch * args.batch_size
+        end = start + args.batch_size
+        neuron_acts[start:end] = reps
+
+    print(f"accuracy: {np.mean(accs):.3f}")
+    neuron_masks = mask_threshold(neuron_acts, args.neuron_threshold)
+
+    final_weight = model.l2.weight.detach().numpy()
+    return explain(
+        list(range(args.n_hidden)),
+        neuron_masks,
+        feature_masks,
+        feature_names,
+        final_weight,
+    )
 
 
-def explain(neurons, neuron_masks, all_feature_masks, all_feature_names):
+def explain(neurons, neuron_masks, feature_masks, feature_names, final_weight):
+    records = []
     for i_neuron in neurons:
         neuron_mask = neuron_masks[:, i_neuron]
-        intersection = np.minimum(neuron_mask[:, np.newaxis], all_feature_masks).sum(
-            axis=0
-        )
-        union = np.maximum(neuron_mask[:, np.newaxis], all_feature_masks).sum(axis=0)
+        intersection = (feature_masks & neuron_mask[:, np.newaxis]).sum(axis=0)
+        union = (feature_masks | neuron_mask[:, np.newaxis]).sum(axis=0)
+
         iou = intersection / union
         best = np.argmax(iou)
-        if all_feature_masks[:, best].mean() in (0, 1):
+
+        if feature_masks[:, best].mean() in {0, 1}:
             continue
-        if iou[best] < 0.5:
-            continue
-        print(i_neuron, best, iou[best])
-        print(neuron_mask.mean())
-        print(all_feature_masks[:, best].mean())
-        print(all_feature_names[best])
-        print()
+
+        # Compute final weight
+        weight = final_weight[0, i_neuron]
+        records.append(
+            {
+                "neuron": i_neuron,
+                "feature": feature_names[best],
+                "iou": iou[best],
+                "weight": weight,
+            }
+        )
+        print(
+            f"neuron {i_neuron:03d}: feature {feature_names[best]} (iou {iou[best]:.3f}, weight {weight:.3f})"
+        )
+
+    return pd.DataFrame(records)
 
 
 if __name__ == "__main__":
@@ -218,7 +279,15 @@ if __name__ == "__main__":
     )
 
     parser.add_argument("mode", choices=["train", "analyze"])
+    parser.add_argument("--n_hidden", default=64, type=int)
+    parser.add_argument("--epochs", default=10, type=int)
+    parser.add_argument("--batch_size", default=100, type=int)
+    parser.add_argument("--n_explain", default=2000, type=int)
+    parser.add_argument("--neuron_threshold", default=0.5, type=float)
+    parser.add_argument("--feature_threshold", default=0.5, type=float)
+    parser.add_argument("--max_formula_length", default=2, type=int)
     parser.add_argument("--save_model", default="models/model.pt")
+    parser.add_argument("--save_analysis", default="analysis/neurons.csv")
     parser.add_argument("--train_data", default="data/train.csv")
     parser.add_argument("--test_data", default="data/test.csv")
 
@@ -227,4 +296,6 @@ if __name__ == "__main__":
     if args.mode == "train":
         train(args)
     elif args.mode == "analyze":
-        analyze(args)
+        df = analyze(args)
+        os.makedirs(os.path.split(args.save_analysis)[0], exist_ok=True)
+        df.to_csv(args.save_analysis, index=False)
