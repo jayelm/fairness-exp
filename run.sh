790ba76bfce715f3f6e72099b9335894996d6ef5
Jesse Mu <6245103+jayelm@users.noreply.github.com>
2020-02-27 23:33:10 -0500

    Add a bunch of runs

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+for max_length in 1 2 3 4; do
+    for thresh in 0.04 0.20 0.50; do
+        python code/main.py analyze --max_formula_length $max_length --neuron_threshold $thresh --save_analysis analysis/"$max_length"_"$thresh".csv
+    done
+done
